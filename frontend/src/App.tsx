de0c4b871b93befc857946e4ac80b7f3ff0b64b7
masafumi kato <xslash1105@gmail.com>
2020-12-28 23:42:55 +0900

    [update] added item component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Item from './components/Item';
 import './App.css';
 
 function App() {
+  const images = [
+    'https://unsplash.it/600/400?random',
+    'https://unsplash.it/600/600?random',
+    'https://unsplash.it/720/460?random',
+    'https://unsplash.it/800/400?random',
+    'https://unsplash.it/480/700?random',
+    'https://unsplash.it/680/420?random',
+    'https://unsplash.it/740/480?random',
+    'https://unsplash.it/840/440?random',
+    'https://unsplash.it/640/380?random',
+    'https://unsplash.it/500/680?random',
+  ];
+
   return (
     <div className="App">
+
       <div className="flexbox">
-        <div className="item">
-          <img src="https://unsplash.it/600/400?random"/>
-          <p className="title">1st item</p>
-        </div>
-        <div className="item">
-          <img src="https://unsplash.it/600/600?random"/>
-          <p className="title">2nd item</p>
-        </div>
-        <div className="item">
-          <img src="https://unsplash.it/720/460?random"/>
-          <p className="title">3rd item</p>
-        </div>
-        <div className="item">
-          <img src="https://unsplash.it/800/400?random"/>
-          <p className="title">4th item</p>
-        </div>
-        <div className="item">
-          <img src="https://unsplash.it/480/700?random"/>
-          <p className="title">5th item</p>
-        </div>
-        <div className="item">
-          <img src="https://unsplash.it/680/420?random"/>
-          <p className="title">6th item</p>
-        </div>
-        <div className="item">
-          <img src="https://unsplash.it/740/480?random"/>
-          <p className="title">7th item</p>
-        </div>
-        <div className="item">
-          <img src="https://unsplash.it/840/440?random"/>
-          <p className="title">8th item</p>
-        </div>
-        <div className="item">
-          <img src="https://unsplash.it/640/380?random"/>
-          <p className="title">9th item</p>
-        </div>
-        <div className="item">
-          <img src="https://unsplash.it/500/680?random"/>
-          <p className="title">10th item</p>
-        </div>
+        {images.map((image, i) => (
+          <Item title={i+1+" item"} imageSrc={image} index={i} />
+        ))}
       </div>
     </div>
   );
