cb6d24f762aee63308b852c7968eb4975da583f7
masafumi kato <xslash1105@gmail.com>
2020-12-28 23:03:02 +0900

    [add] masonry design added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,48 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flexbox">
+        <div className="item">
+          <img src="https://unsplash.it/600/400?random"/>
+          <p className="title">1st item</p>
+        </div>
+        <div className="item">
+          <img src="https://unsplash.it/600/600?random"/>
+          <p className="title">2nd item</p>
+        </div>
+        <div className="item">
+          <img src="https://unsplash.it/720/460?random"/>
+          <p className="title">3rd item</p>
+        </div>
+        <div className="item">
+          <img src="https://unsplash.it/800/400?random"/>
+          <p className="title">4th item</p>
+        </div>
+        <div className="item">
+          <img src="https://unsplash.it/480/700?random"/>
+          <p className="title">5th item</p>
+        </div>
+        <div className="item">
+          <img src="https://unsplash.it/680/420?random"/>
+          <p className="title">6th item</p>
+        </div>
+        <div className="item">
+          <img src="https://unsplash.it/740/480?random"/>
+          <p className="title">7th item</p>
+        </div>
+        <div className="item">
+          <img src="https://unsplash.it/840/440?random"/>
+          <p className="title">8th item</p>
+        </div>
+        <div className="item">
+          <img src="https://unsplash.it/640/380?random"/>
+          <p className="title">9th item</p>
+        </div>
+        <div className="item">
+          <img src="https://unsplash.it/500/680?random"/>
+          <p className="title">10th item</p>
+        </div>
+      </div>
     </div>
   );
 }
